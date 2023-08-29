dc849bcf6aa9844d20015c21b1c7ed4f775246f6
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-08-29 18:28:12 +0600

    ADDED product interface

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,3 +1,28 @@
-export type IProducts = {
-    
+import { Types } from 'mongoose'
+
+export type ICategory =
+  | 'Electronics'
+  | 'Fashion'
+  | 'Sports'
+  | 'Gifts'
+  | 'Garden'
+  | 'Kitchen'
+  | 'Furniture'
+
+export type IFeatures = {
+  description: string
+  value: string
+}
+export type IProduct = {
+  name: string
+  description: string
+  category: ICategory
+  features: IFeatures[]
+  img: string[]
+  price: number
+  vendor: string
+  review?: Types.ObjectId
+  stock: 'available' | 'unavailable'
+  rating?: '1' | '2' | '3' | '4' | '5'
+  quantity: number
 }
