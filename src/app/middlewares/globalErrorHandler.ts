8b3999f3c7a005724fa49a115a102a71a267b0ca
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-08-29 09:48:13 +0600

    ADDED: to vercel for deploy

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express'
 
 export const globalErrorHandler = (
-  err,
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
-import { errorlogger, logger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-
-    logger.info('database connected')
+    console.log('database connected')
     app.listen(config.port, () => {
-      logger.info(`listening on port ${config.port}`)
+      console.log(`listening on port ${config.port}`)
     })
   } catch (error) {
-    errorlogger.error('failed to connect')
+    console.log('failed to connect')
   }
 }
 
